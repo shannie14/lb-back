4544093d2e9fe24ee4dcb80d9331abb8d78de793
Shannon Kendall <shannon.marie.kendall@gmail.com>
2025-06-08 13:48:37 -0400

    brand

--- routes/salesRoutes.js
+++ routes/salesRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express')
 
-const { getTopMonthlySellers, getGrossByYearAndType } = require('../controllers/sales')
+const { getTopMonthlySellers, getGrossByYearAndType, getGrossByYearAndBrand } = require('../controllers/sales')
 
 const router = express.Router()
 
 router.get('/top-sellers', getTopMonthlySellers)
 router.get('/annual-type', getGrossByYearAndType)
+router.get('/annual-brand', getGrossByYearAndBrand)
 
 module.exports = router
